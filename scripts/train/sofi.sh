4572dbf998d77c3a84de0516c1be3405f5be7db7
SebastianJanampa <rjsebastian001@gmail.com>
2024-11-19 11:43:12 -0700

    fix .sh file

--- scripts/train/sofi.sh
+++ scripts/train/sofi.sh
@@ -6,4 +6,4 @@ if [[ $# -eq 0 ]] ; then
 fi
 dataset=$1
 
-python main.py --output_dir logs/$dataset/sofi --num_feature_levels 2 --batch_size 8 --print_freq 400 --lr 2e-4 --lr_backbone 2e-5 --dec_n_points 8 --enc_n_points 32 --resolution high --num_workers 4 --amp --dataset  $dataset
+python main.py --output_dir logs/$dataset/sofi --num_feature_levels 2 --batch_size 8 --print_freq 400 --lr 2e-4 --lr_backbone 2e-5 --dec_n_points 8 --enc_n_points 32 --resolution high --num_workers 4 --dataset  $dataset
